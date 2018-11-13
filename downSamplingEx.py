4b060995a128967bf14a10f2de71bcd17e78ab3b
svendsen991 <emilabildgaardsvendsen@gmail.com>
2018-11-13 12:27:21 +0100

    Downsampling

--- downSamplingEx.py
+++ downSamplingEx.py
@@ -6,27 +6,35 @@ from scipy import signal, fftpack
 import numpy as np
 import matplotlib.pyplot as plt
 
+## Sampling rate
 fs = 8000
-##Bdown = signal.firwin(159, 1250, fs=fs, width=4)
+## Number of samples
 N = 2048
-
-##L = 3
+## Downsampling factor
 M = 2
-n = np.arange(0, N-1, 1)
 
+n = np.arange(0, N-1, 1)
+f = (np.arange(0, (N/2) - 1, 1) * fs / N)
+
+## Input
 x = 5 * np.sin((np.pi / 4) * n ) + np.cos((5*np.pi*n) / 8)
 
+## Single sided amplitude spectrum
 X = 2*np.abs(fftpack.fft(x, N)) / N
 X[0] = X[0] / 2
 
-f = (np.arange(0, (N/2) - 1, 1) * fs / N)
 
+## Output with sampling rate fs / M
 y = x[np.arange(0, N, M)]
 NM = len(y)
-Y = 2*np.abs(fftpack.fft(y, NM)) / NM
-Y[0] = Y[0] / 2
 fsM = np.arange(0, (NM/2)-1, 1) * (fs/M)/NM
 
+## Single sided amplitude spectrum
+Y = 2*np.abs(fftpack.fft(y, NM)) / NM
+Y[0] = Y[0] / 2
+
+
+## Plot both spectrums
 fig = plt.figure()
 plt.title('Digital filter frequency response')
 ax1 = fig.add_subplot(111)
